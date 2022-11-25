c49dda63c9366e7f96ff15bee1357617dae3a52a
artem <artemmurashov74@gmail.com>
2022-11-25 16:23:32 +0500

    End creating form, working form

--- Lab1.py
+++ Lab1.py
@@ -1,14 +1,30 @@
 import sys
+from PyQt5 import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from Information import Ui_MainWindow
-
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
         self.setupUi(self)
 
+        # self.lineEdit.pressed.connect(self.label)
+        # self.lineEdit_2.pressed.connect(self.label_2)
+        # self.pushButton.pressed.connect(self.operations)
+        # self.show()
+
+        def display(self):
+            self.lcdNumber.display(self.stack[-1])
+
+        def operations(self):
+            self.c=2+5
+            self.display()
+
+
 if __name__=='__main__':
-    form=QApplication([])
-    form.setApplicationName('Information method')
-    window=MainWindow()
-    form.exec_()
+    app=QApplication(sys.argv)
+    window = MainWindow()
+    app.setApplicationName('Information method')
+    window.show()
+    app.exec_()
